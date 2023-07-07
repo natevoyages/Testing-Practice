afb17834a9bd4c0d39588458dc67e6d539e90b6a
NateVoyages <allanromo20@gmail.com>
2023-07-07 15:07:07 -0700

    Refactor caesar-cipher fn & add capital/punctuation testing

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,19 +1,20 @@
 const caesarCipher = (string, shift) => {
-  const alphaMap = new Map();
-  const createAlphaMap = () => {
-    for(let i = 97; i <= 122; i++){
-      alphaMap.set(String.fromCharCode(i), i);
-    }
-  };
-  createAlphaMap();
   let strArr = string.split('');
   const cipherString = (strArr)=>{
     let newArr = [];
     strArr.forEach((char) => {
-      let charCode = alphaMap.get(char);
-      charCode = (shift + charCode);
-      if (charCode > 122){
-        charCode = ( ( shift + charCode ) % 123 ) + 96;
+      let charCode =  char.charCodeAt(0);
+      if(charCode >= 97 && charCode < 123){
+        charCode = (shift + charCode);
+        if (charCode > 122){
+          charCode = ( charCode  % 123 ) + 97;
+        }
+      }
+      else if (charCode >= 65  && charCode <= 90){
+        charCode = (shift + charCode);
+        if (charCode > 90){
+          charCode = ( charCode  % 91 ) + 65;
+        }
       }
       newArr.push(String.fromCharCode(charCode));
 
--- caesar-cipher.test.js
+++ caesar-cipher.test.js
@@ -1,5 +1,13 @@
 import { caesarCipher } from "./caesar-cipher";
 
-test('ciphers', () => expect(caesarCipher('abc', 1)).toBe('bcd'));
+test('ciphers lower', () => expect(caesarCipher('abc', 1)).toBe('bcd'));
 
-test('wrapping', () => expect(caesarCipher('z', 1)).toBe('a'));
+test('ciphers upper', () => expect(caesarCipher('ABC', 1)).toBe('BCD'));
+
+test('wrapping lower', () => expect(caesarCipher('z', 1)).toBe('a'));
+
+test('wrapping upper', () => expect(caesarCipher('Z', 1)).toBe('A'));
+
+test('return punctuation', () => expect(caesarCipher('.', 1)).toBe('.'));
+
+test('test capitalization', () => expect(caesarCipher('John', 5)).toBe('Otms'));
