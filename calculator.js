2028379ceebfa6c9ea0d40b9db567aec1fb603de
NateVoyages <allanromo20@gmail.com>
2023-07-06 19:57:58 -0700

    Edit calculator testing

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-const calculator = (a, b) => {
-    const subtract = () => a - b;
-    const add = () => a + b;
-    const multiply = () => a * b;
-    const divide = () => a / b;
+const calculator = () => {
+    const subtract = (a, b) => a - b;
+    const add = (a, b) => a + b;
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => a / b;
     
     return {subtract, add, multiply, divide};
 }
--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,17 @@
 import{calculator} from './calculator';
 
 test('addition', () => {
-    expect(calculator(5,6).add()).toBe(11);
+    expect(calculator().add(5,6)).toBe(11);
 });
 
 test('subtract', () => {
-    expect(calculator(5,6).subtract()).toBe(-1);
+    expect(calculator().subtract(5,6)).toBe(-1);
 });
 
 test('multiply', () => {
-    expect(calculator(5,6).multiply()).toBe(30);
+    expect(calculator().multiply(5,6)).toBe(30);
 });
 
 test('divide', () => {
-    expect(calculator(5,6).divide()).toBeCloseTo(0.83);
+    expect(calculator().divide(5,6)).toBeCloseTo(0.83);
 });
