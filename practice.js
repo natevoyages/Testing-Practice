59e6ac175759c94295930e96bcb7b2381d922103
NateVoyages <allanromo20@gmail.com>
2023-07-06 19:07:40 -0700

    Add reverseString testing

--- practice.js
+++ practice.js
@@ -3,6 +3,18 @@ const capitalize = (string) => {
   strArr[0] = strArr[0].toUpperCase();
   let newString = strArr.join('');
   return newString;
-}
+};
 
-export {capitalize};
+const reverseString = (str) => {
+  let strArr = str.split('');
+  for (let i = 0; i < (strArr.length - 1) / 2; i++) {
+    let holder = strArr[i];
+    strArr[i] = strArr[strArr.length - 1 - i];
+    strArr[strArr.length - 1 - i] = holder;
+  }
+  let newArr = strArr.join('');
+
+  return newArr;
+};
+
+export { capitalize, reverseString };
--- practice.test.js
+++ practice.test.js
@@ -1,6 +1,13 @@
-import {capitalize} from './practice';
-
+import { capitalize, reverseString } from './practice';
 
 test('capitalize', () => {
-  expect(capitalize('brian')).toBe('Brian')
+  expect(capitalize('brian')).toBe('Brian');
+});
+
+test('reverse string short', () => {
+  expect(reverseString('dog')).toBe('god');
+});
+
+test('reverse string longer', () => {
+  expect(reverseString('brandon')).toBe('nodnarb');
 });
